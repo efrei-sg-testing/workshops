7bcd08a51ff485f05efa5cdd35908bf8b844d6ca
Maxence Aïci <maxence.aici@alan.eu>
2023-07-17 12:34:16 +0200

    correction exo 3.1

--- first-tests/3-Hangman/hangman.js
+++ first-tests/3-Hangman/hangman.js
@@ -1,4 +1,23 @@
+const MAX_TRIES = 10;
+
 export function hangmanState(wordToGuess, guessedLetters) {
+  const uppercaseWordToGuess = wordToGuess.toUpperCase();
+  const uppercaseGuessedLetters = guessedLetters
+    .toUpperCase()
+    .replaceAll(" ", "");
+
+  let remainingLetters = uppercaseWordToGuess;
+  let wrongLetters = 0;
+
+  for (const guessedLetter of uppercaseGuessedLetters) {
+    const isValidLetter = uppercaseWordToGuess.includes(guessedLetter);
+    if (!isValidLetter) wrongLetters++;
+    else remainingLetters = remainingLetters.replaceAll(guessedLetter, "");
+
+    if (wrongLetters >= MAX_TRIES) return "lose";
+    if (remainingLetters === "") return "win";
+  }
+
   return "in_progress";
 }
 
